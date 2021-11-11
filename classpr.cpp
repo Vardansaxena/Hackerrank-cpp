0d1f2c7dc753d96d40afb16259a377aab8276d17
Vardan saxena <35631613+Vardansaxena@users.noreply.github.com>
2021-11-11 17:55:47 +0530

    Add files via upload

---
+++ classpr.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+#include<string>
+#include <sstream>
+using namespace std;
+class namedStudent{
+	private :
+	int age , standard;
+	string first_name, last_name;
+	//int a , s, f, l ;
+	public: 
+	void set_age(int a){
+		age = a;
+	}
+	void set_standard(int s){
+		standard = s;
+	}
+	void set_first_name(string f){
+		first_name = f;
+	}
+	void set_last_name(string l){
+		last_name = l;
+	}
+	int get_age(){
+		return age;
+	}
+	string get_first_name(){
+		return first_name;
+	}
+	string get_last_name(){
+		return last_name;
+	}
+	int get_standard(){
+		return standard;
+	}
+	string to_string(){
+	     string a,s;
+        stringstream aa,ss;
+         aa<<age;
+         ss<<standard;
+         a=aa.str();
+         s=ss.str();
+        return  a+","+ first_name +","+ last_name +","+ s;
+        }
+	
+};
+int main()
+{
+	int age , standard;
+	string first_name, last_name;
+	cin>> age >> first_name >> last_name >> standard;
+	
+	namedStudent st;
+	st.set_age(age);
+	st.set_standard(standard);
+	st.set_first_name(first_name);
+	st.set_last_name(last_name);
+	
+	cout << st.get_age() << "\n";
+    cout << st.get_last_name() << ", " << st.get_first_name() << "\n";
+    cout << st.get_standard() << "\n";
+    cout << "\n";
+    cout << st.to_string();
+    
+    return 0;
+    
+	
+}
