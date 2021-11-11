dae97b0cbbd9c57ef0d3fdb04d00b24b9503ccc1
Vardan saxena <35631613+Vardansaxena@users.noreply.github.com>
2021-11-11 17:51:43 +0530

    Add files via upload

---
+++ outp.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+
+int main()
+{
+	string inputString;
+	
+	getline(cin, inputString);
+	cout<<"Hello, World."<<endl;
+	cout<<inputString;
+	return 0;
+}
